f868d38fbbeec95fd65fb713d752c41e44d6083b
Julie Norris <julienorris0715@gmail.com>
2023-03-13 14:19:17 -0700

    Update SQL_Scripts.py
    updated dates

--- SQL_Scripts.py
+++ SQL_Scripts.py
@@ -15,81 +15,7 @@ LEFT OUTER JOIN REENROLLMENTS R ON S.ID = R.STUDENTID
 WHERE
 	R.EXITDATE > R.ENTRYDATE 
 	AND S.STUDENT_NUMBER IN (
-187251,
-300397,
-397888,
-391330,
-391591,
-391831,
-324588,
-330133,
-329887,
-390830,
-329406,
-329894,
-358266,
-324766,
-328664,
-328913,
-325122,
-324331,
-328968,
-385742,
-325267,
-329177,
-329917,
-331361,
-330598,
-322125,
-328199,
-350088,
-331183,
-334994,
-318523,
-334000,
-382960,
-324770,
-324406,
-374890,
-335357,
-385663,
-402527,
-335471,
-396930,
-337157,
-379961,
-333966,
-334228,
-3631000232,
-334877,
-325205,
-334477,
-395767,
-374215,
-330086,
-329441,
-379050,
-330911,
-334359,
-350731,
-331988,
-327342,
-329965,
-334524,
-352797,
-395955,
-327711,
-324697,
-334283,
-398213,
-334784,
-363408,
-334120,
-334605,
-386729,
-329392,
-337113,
-330009
+XXXXlist of student numbersXXX
 )
 '''
 
@@ -105,8 +31,8 @@ FROM
 	PS_adaadm_defaults_all ada 
 WHERE
 	ada.studentid = s.id 
-	AND ada.calendardate BETWEEN TO_DATE( '08/01/2014', 'mm/dd/yyyy' ) 
-	AND TO_DATE( '03/09/2018', 'mm/dd/yyyy' )
+	AND ada.calendardate BETWEEN TO_DATE( '08/01/2021', 'mm/dd/yyyy' ) 
+	AND TO_DATE( '03/09/2022', 'mm/dd/yyyy' )
 	) Days_Enrolled,
 	(
 SELECT
@@ -129,8 +55,8 @@ SELECT
 FROM
 	ps_attendance_meetinter pm 
 WHERE
-	att_date BETWEEN TO_DATE( '08/01/2014', 'mm/dd/yyyy' ) 
-	AND TO_DATE( '03/09/2018', 'mm/dd/yyyy' )  
+	att_date BETWEEN TO_DATE( '08/01/2021', 'mm/dd/yyyy' ) 
+	AND TO_DATE( '03/09/2022', 'mm/dd/yyyy' )  
 	AND att_code IN ( 'U', 'A', 'I', 'M', 'E', 'F', 'K', 'S', 'C' ) 
 	AND pm.studentID = s.ID 
 GROUP BY
