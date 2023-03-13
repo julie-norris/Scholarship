fc6cd3671ec306df38ed5f9571b20b71a8a3058f
Julie Norris <julienorris0715@gmail.com>
2023-03-13 14:15:05 -0700

    Attendance script - not in use

---
+++ RPPositiveAttendance.py
@@ -0,0 +1,57 @@
+""" This requirement was eliminated in 2018-19"""
+
+import cx_Oracle
+# I put all the sql queries into a different file to clean it up a little bit
+from  SQL_Scripts import sql_attendance
+
+conn_str = 'psnavigator/navigate@172.24.12.211:1521/CA528'
+conn = cx_Oracle.connect(conn_str)
+c = conn.cursor()
+
+import pandas as pd
+import numpy as np
+import sys
+
+
+import os
+
+cwd=os.getcwd()
+os.chdir("C:\\Oracle\\RichmondPromise")
+
+
+def positive_attendance(query):
+	'''creates an object of student attendance data'''
+	results = c.execute(query)
+	attendance= {}
+	for row in results:
+		student_number =int(row[0])
+		attendance[student_number]=StudentAttendance(student_number, int(row[3]), int(row[4]), int(row[5]) )		
+	return (attendance)
+	
+class StudentAttendance:
+	'''creates record for the attendance of a student'''
+	
+	def __init__(self, student_number, days_enrolled, days_present, days_absent):
+		self.student_number = student_number
+		self.days_enrolled = days_enrolled
+		self.days_present = days_present
+		self.days_absent = days_absent
+		
+				
+	def calculate_attendance(self):
+    		#return float(self.days_present/self.days_enrolled )
+		return {'Student_Number': self.student_number, 'Attendance%': (self.days_present/self.days_enrolled)}
+	
+attendance = positive_attendance(sql_attendance)
+
+#for student_number, StudentAttendance in attendance.items():
+	#	results =  ('%s %s' % (StudentAttendance.student_number, StudentAttendance.calculate_attendance()))
+		#print (results)
+		
+df=pd.DataFrame([a.calculate_attendance() for a in attendance.values()])
+#, index=index, columns=['Student_Number', 'AttendancePercentage'])
+writer=pd.ExcelWriter('Attendance1819.xlsx')
+
+#writes the new df to excel
+df.to_excel(writer, engine=engine)
+
